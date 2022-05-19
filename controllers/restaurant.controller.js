0081ebf2bfc15f995f265ae2ed4740fafd6d6bb8
Magyar-Hunor Tamas <hunor@makers.tech>
2022-05-19 11:51:12 +0100

    Delete controller logic to controller, update routing

---
+++ controllers/restaurant.controller.js
@@ -0,0 +1,45 @@
+const Restaurant = require('../models/Restaurant');
+
+// Create and Save new Restaurant
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.name) {
+        res.status(400).send({ message: "Restaurant name cant't be empty!"})
+        return;
+    }
+
+    // Create a Restaurant
+   Restaurant.create(req.body)
+      .then(restaurant => res.json({ msg: 'Restaurant added successfully' }))
+      .catch(err => res.status(400).json({ error: 'Unable to add this Restaurant' }));
+}
+
+// Retrieve all Restaurants from the database
+exports.findAll = (req, res) => {
+    Restaurant.find()
+      .then(restaurants => res.json(restaurants))
+      .catch(err => res.status(404).json({ norestaurantsfound: 'No Restaurants found'}));
+}
+
+// Find a single Restaurant with an id
+exports.findOne = (req, res) => {
+    Restaurant.findById(req.params.id)
+      .then(restaurant => res.json(restaurant))
+      .catch(err => res.status(404).json({ norestaurantfound: 'No Restaurant found'}));
+}
+
+// Update a Restaurant by the id in the request
+exports.update = (req, res) => {
+    Restaurant.findByIdAndUpdate(req.params.id, req.body)
+        .then(restaurant => res.json({ msg: 'Restaurant updated successfully' }))
+        .catch(err =>
+            res.status(400).json({ error: 'Unable to update Restaurant in the database' }));
+}
+
+// Delete a Restaurant with the specified id in the request
+exports.delete = (req, res) => {
+    Restaurant.findByIdAndRemove(req.params.id, req.body)
+      .then(restaurant => res.json({ mgs: `${restaurant} -> Restaurant record deleted successfully` }))
+      .catch(err => res.status(404).json({ error: 'No such restaurant' }));
+}
+
--- routes/api/restaurants.js
+++ routes/api/restaurants.js
@@ -3,6 +3,9 @@
 const express = require('express');
 const router = express.Router();
 
+// Load restaurants controller
+const restaurants = require('../../controllers/restaurant.controller');
+
 // Load Restaurant model
 const Restaurant = require('../../models/Restaurant');
 
@@ -14,48 +17,26 @@ router.get('/test', (req, res) => res.send('restaurant route testing'));
 // @route GET api/restaurants
 // @description Get all Restaurants
 // @access Public
-router.get('/', (req, res) => {
-    Restaurant.find()
-      .then(restaurants => res.json(restaurants))
-      .catch(err => res.status(404).json({ norestaurantsfound: 'No Restaurants found'}));
-});
+router.get('/', restaurants.findAll);
 
 // @route GET api/restaurants/:id
 // @description Get single restaurant by id
 // @access Public
-router.get('/:id', (req, res) => {
-    Restaurant.findById(req.params.id)
-      .then(restaurant => res.json(restaurant))
-      .catch(err => res.status(404).json({ norestaurantfound: 'No Restaurant found'}));
-})
+router.get('/:id', restaurants.findOne);
 
 // @route POST api/restaurants
 // @description add/save Restaurant
 // @access Public
-router.post('/', (req, res) => {
-    Restaurant.create(req.body)
-      .then(restaurant => res.json({ msg: 'Restaurant added successfully' }))
-      .catch(err => res.status(400).json({ error: 'Unable to add this Restaurant' }));
-  });
+router.post('/', restaurants.create);
   
-  // @route PUT api/restaurants/:id
-  // @description Update Restaurant
-  // @access Public
-  router.put('/:id', (req, res) => {
-    Restaurant.findByIdAndUpdate(req.params.id, req.body)
-      .then(restaurant => res.json({ msg: 'Restaurant updated successfully' }))
-      .catch(err =>
-        res.status(400).json({ error: 'Unable to update Restaurant in the database' })
-      );
-  });
+// @route PUT api/restaurants/:id
+// @description Update Restaurant
+// @access Public
+router.put('/:id', restaurants.update);
   
-  // @route DELETE api/restaurant/:id
-  // @description Delete Restaurant by id
-  // @access Public
-  router.delete('/:id', (req, res) => {
-    Restaurant.findByIdAndRemove(req.params.id, req.body)
-      .then(restaurant => res.json({ mgs: 'Restaurant record deleted successfully' }))
-      .catch(err => res.status(404).json({ error: 'No such restaurant' }));
-  });
+// @route DELETE api/restaurant/:id
+// @description Delete Restaurant by id
+// @access Public
+router.delete('/:id', restaurants.delete);
   
-  module.exports = router;
+module.exports = router;
